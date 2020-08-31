1bf84290c6664e17988602d24060e1501f7c0ac1
Mark A <cw.prime@gmail.com>
2020-08-30 22:46:57 -0400

    gfgf

--- keystone.js
+++ keystone.js
@@ -57,7 +57,9 @@ keystone.set('locals', {
 app.listen(process.env.PORT || 3000);
 // Load your project's Routes
 
-
+app.get('/', function (request, response) {
+	response.send('Hello World!');
+});
 keystone.set('routes', require('./routes'));
 
 
--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ var routes = {
 	views: importRoutes('./views'),
 };
 
-var express = require('express');  
+var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
