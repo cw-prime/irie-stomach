c824ff42ec708e255d47ecc12a7007e6d79d529a
Mark B <cw.prime@gmail.com>
2023-01-15 10:04:05 -0500

    first committ

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('MONGODB_URL=mongodb://Nanny:54lTICV7mPjUYZcc@cluster0-shard-00-00.pil78.mongodb.net:27017,cluster0-shard-00-01.pil78.mongodb.net:27017,cluster0-shard-00-02.pil78.mongodb.net:27017/irie_blog?ssl=true&replicaSet=atlas-pl0pps-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('MONGODB_URL=mongodb://Nanny:h7RBnU7ZaLrxUCnY@cluster0-shard-00-00.pil78.mongodb.net:27017,cluster0-shard-00-01.pil78.mongodb.net:27017,cluster0-shard-00-02.pil78.mongodb.net:27017/irie_blog?ssl=true&replicaSet=atlas-pl0pps-shard-0&authSource=admin&retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
